f80261ac24489366c94b2d65a560989b1f3d9e66
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-06-09 18:31:48 +0300

    Выполнен урок 11

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#text');
+const square = document.querySelector('#square');
+const circle = document.querySelector('#circle');
+const e_btn = document.querySelector('#e_btn');
+const range = document.querySelector('#range');
+const span = document.querySelector('#range-span');
+
+const changeColor = function () {
+    square.style.backgroundColor = input.value;
+    input.value = '';
+};
+
+const changeCircle = function (event) {
+    span.textContent = event.target.value;
+    circle.style.height = event.target.value + '%';
+    circle.style.width = event.target.value + '%';
+};
 
 
+btn.addEventListener('click', changeColor);
 
 
+e_btn.style.display = 'none';
 
+
+range.addEventListener('input', changeCircle);
+range.addEventListener('change', changeCircle);
